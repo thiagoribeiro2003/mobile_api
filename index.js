5cac65a6eb13f013e9cf4be190e6fbbcf9d0d9b3
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2023-02-09 12:00:07 -0300

    Criando API de evento

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-import express  from "express";
+import express from "express";
 import cors from "cors";
-import {ler, inserir, lerUm, atualizar, excluir} from "./src/aluno.js";
+import {
+  ler,
+  inserir,
+  lerUm,
+  atualizar,
+  excluir,
+  lerEvento,
+  inserirEvento,
+  lerUmEvento,
+  atualizarEvento,
+  excluirEvento,
+} from "./src/aluno.js";
 
 const app = express();
 const porta = process.env.PORT || 3000;
@@ -11,57 +22,96 @@ app.use(cors());
 // Configurando suporte a JSON
 app.use(express.json()); // essa linha dentro do express é o que dará suporte a JSON
 // Configurando suporte a dados de formulários (input)
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 //configurando ROTAS
 
 // Rota (endpoint) para a raiz da api
-app.get('/', (req, res) => {
-    res.send('Projeto Mobile Api');
+app.get("/", (req, res) => {
+  res.send("Projeto Mobile Api");
 });
 
-// Rota (endpoint para exibir todos os alunos)
-app.get('/perfil', (req, res) => {
-   // res.send(`Todos alunos`);
-   ler(res);
+// Rota (endpoint para exibir todos os perfils)
+app.get("/perfil", (req, res) => {
+  // res.send(`Todos alunos`);
+  ler(res);
 });
 
-// Rota (endpoint) para exibir um único aluno
+// Rota (endpoint para exibir todos os eventos)
+app.get("/evento", (req, res) => {
+  // res.send(`Todos alunos`);
+  lerEvento(res);
+});
+
+// Rota (endpoint) para exibir um único perfil
 app.get(`/perfil/:id`, (req, res) => {
-    // res.send(`Exibindo dados de um aluno`);
-    const id = req.params.id; //params é função nativa do mysql 2 - pegue todos os parâmetros e busque o id
-    lerUm(id, res);
+  // res.send(`Exibindo dados de um aluno`);
+  const id = req.params.id; //params é função nativa do mysql 2 - pegue todos os parâmetros e busque o id
+  lerUm(id, res);
 });
 
-// Rota (endpoint) para INSERIR alunos
-app.post('/perfil', (req, res) => {
-    // res.send(`INSERINDO perfil`);
-    const novoAluno = req.body;
-    inserir(novoAluno, res);
+// Rota (endpoint) para exibir um único evento
+app.get(`/evento/:id`, (req, res) => {
+  // res.send(`Exibindo dados de um aluno`);
+  const id = req.params.id; //params é função nativa do mysql 2 - pegue todos os parâmetros e busque o id
+  lerUmEvento(id, res);
 });
 
-// Rota para atualizar TODOS os dados do aluno
-app.put('/perfil/:id', (req, res) => {
-    res.send(`ATUALIZANDO TODOS os dados dos perfil`);
+// Rota (endpoint) para INSERIR perfil
+app.post("/perfil", (req, res) => {
+  // res.send(`INSERINDO perfil`);
+  const novoAluno = req.body;
+  inserir(novoAluno, res);
 });
 
-app.patch('/perfil/:id', (req, res) => {
-    // res.send(`ATUALIZA ALGUNS/todos os dados de um aluno`);
+// Rota (endpoint) para INSERIR evento
+app.post("/evento", (req, res) => {
+  const novoAluno = req.body;
+  inserirEvento(novoAluno, res);
+});
 
-    // capturar id
-    const id = parseInt(req.params.id);
-    // dados do aluno
-    const aluno = req.body;
-    atualizar(id, aluno, res);
+// Rota para atualizar TODOS os dados do perfil
+app.put("/perfil/:id", (req, res) => {
+  res.send(`ATUALIZANDO TODOS os dados dos perfil`);
+});
+
+app.put("/evento/:id", (req, res) => {
+  res.send(`ATUALIZANDO TODOS os dados dos eventos`);
+});
+
+app.patch("/perfil/:id", (req, res) => {
+  // res.send(`ATUALIZA ALGUNS/todos os dados de um aluno`);
+
+  // capturar id
+  const id = parseInt(req.params.id);
+  // dados do aluno
+  const aluno = req.body;
+  atualizar(id, aluno, res);
+});
+
+app.patch("/evento/:id", (req, res) => {
+  // res.send(`ATUALIZA ALGUNS/todos os dados de um aluno`);
+
+  // capturar id
+  const id = parseInt(req.params.id);
+  // dados do aluno
+  const aluno = req.body;
+  atualizar(id, aluno, res);
 });
 
 // Rota (endpoint) para EXCLUIR aluno
-app.delete('/perfil/:id', (req, res) => {
-    // res.send(`EXCLUI aluno.`);
-    const id = parseInt(req.params.id);
-    excluir(id, res);
+app.delete("/perfil/:id", (req, res) => {
+  // res.send(`EXCLUI aluno.`);
+  const id = parseInt(req.params.id);
+  excluir(id, res);
 });
 
-// Configurando o servidor 
+app.delete("/evento/:id", (req, res) => {
+  // res.send(`EXCLUI aluno.`);
+  const id = parseInt(req.params.id);
+  excluir(id, res);
+});
+
+// Configurando o servidor
 app.listen(porta, () => {
-    console.log('Servidor Express rodando...')
-})
+  console.log("Servidor Express rodando...");
+});
--- src/aluno.js
+++ src/aluno.js
@@ -2,91 +2,169 @@ import conexao from "./banco.js";
 
 // Função que lê a tabela de alunos do BD
 function ler(res) {
-// Criando o CRUD
-const sql = "SELECT * FROM perfil";
+  // Criando o CRUD
+  const sql = "SELECT * FROM perfil";
 
-
-// conectando ao BD
-conexao.query(sql, (erro, resultados) => {
-    if(resultados.length === 0) {
-        res.status(204).end(); //204 = Sem conteúdo. O método .end() para qualquer comunicação.
-        return; // equivalente ao die()
-    } 
-    if(erro) {
-        res.status(400).json(erro.code); // 400 BAD Request - comunicação não entendida pelo servidor / requisição inválida.
-    } else {
-        res.status(200).json(resultados); //deu certo, exibir os resultados
+  // conectando ao BD
+  conexao.query(sql, (erro, resultados) => {
+    if (resultados.length === 0) {
+      res.status(204).end(); //204 = Sem conteúdo. O método .end() para qualquer comunicação.
+      return; // equivalente ao die()
     }
-}) //query equivale ao  execute do php
+    if (erro) {
+      res.status(400).json(erro.code); // 400 BAD Request - comunicação não entendida pelo servidor / requisição inválida.
+    } else {
+      res.status(200).json(resultados); //deu certo, exibir os resultados
+    }
+  }); //query equivale ao  execute do php
+}
+
+function lerEvento(res) {
+  const sql = "SELECT * FROM evento";
+
+  conexao.query(sql, (erro, resultados) => {
+    if (resultados.length === 0) {
+      res.status(204).end(); //204 = Sem conteúdo. O método .end() para qualquer comunicação.
+      return; // equivalente ao die()
+    }
+    if (erro) {
+      res.status(400).json(erro.code); // 400 BAD Request - comunicação não entendida pelo servidor / requisição inválida.
+    } else {
+      res.status(200).json(resultados); //deu certo, exibir os resultados
+    }
+  });
 }
 
 // Inserindo alunos
 
 function inserir(perfil, res) {
-    // Estamos iniciando o comando de inserção de dados no banco utilizando um caracter curinga do MySQL2 "SET ?" que recebe os dados e atribui na ordem sem a necessidade de especificar as colunas. Proteção contra injection e tratamento de strings 
-    const sql = "INSERT INTO perfil SET ?";
+  // Estamos iniciando o comando de inserção de dados no banco utilizando um caracter curinga do MySQL2 "SET ?" que recebe os dados e atribui na ordem sem a necessidade de especificar as colunas. Proteção contra injection e tratamento de strings
+  const sql = "INSERT INTO perfil SET ?";
 
-    conexao.query(sql, perfil, (erro) => {
-        if(erro){
-            res.status(400).json(erro.code); //400 - requisição inválida e informa o código de erro.
-        } else {
-            res.status(201).json({"status": "perfil inserido"}); // 201- criado e apresenta a mensagem do Aluno inserido!. 
-            //res.status(201).end();
-        }
-    });
+  conexao.query(sql, perfil, (erro) => {
+    if (erro) {
+      res.status(400).json(erro.code); //400 - requisição inválida e informa o código de erro.
+    } else {
+      res.status(201).json({ status: "perfil inserido" }); // 201- criado e apresenta a mensagem do Aluno inserido!.
+      //res.status(201).end();
+    }
+  });
+}
+
+function inserirEvento(evento, res) {
+  const sql = "INSERT INTO evento SET ?";
+
+  conexao.query(sql, evento, (erro) => {
+    if (erro) {
+      res.status(400).json(erro.code); //400 - requisição inválida e informa o código de erro.
+    } else {
+      res.status(201).json({ status: "evento inserido" }); // 201- criado e apresenta a mensagem do Aluno inserido!.
+      //res.status(201).end();
+    }
+  });
 }
 
 function lerUm(email, res) {
-    const sql = "SELECT * FROM perfil WHERE email = ?";
+  const sql = "SELECT * FROM perfil WHERE email = ?";
 
   conexao.query(sql, email, (erro, resultados) => {
-    if(resultados.length === 0){
-        res.status(204).end();
-        return;
+    if (resultados.length === 0) {
+      res.status(204).end();
+      return;
     }
     // if erro ou resultado
-    if(erro){
-        res.status(400).json(erro.code);
+    if (erro) {
+      res.status(400).json(erro.code);
     } else {
-        res.status(200).json(resultados[0]);
+      res.status(200).json(resultados[0]);
     }
-  })
+  });
+}
+
+function lerUmEvento(id, res) {
+  const sql =
+    "SELECT * FROM evento LEFT JOIN perfil ON evento.perfil_id = perfil.id WHERE perfil_id = ?";
+  conexao.query(sql, id, (erro, resultados) => {
+    if (resultados.length === 0) {
+      res.status(204).end();
+      return;
+    }
+    // if erro ou resultado
+    if (erro) {
+      res.status(400).json(erro.code);
+    } else {
+      res.status(200).json(resultados[0]);
+    }
+  });
 }
 
 // Atualizar aluno
 // Essa função vai receber um id, os dados do aluno, e res.
 function atualizar(email, perfil, res) {
-    const sql = "UPDATE perfil SET ? WHERE email = ?";
+  const sql = "UPDATE perfil SET ? WHERE email = ?";
 
-    // Aqui a ordem é importante, para passar mais de um parâmetro usamos o array. Dentro dele a ordem importa, pois precisa corresponder ao SQL acima.
-    conexao.query(sql, [perfil, email], (erro, resultados) => {
-        if(erro) {
-            res.status(400).json(erro.code);
-        } else {
-            // res.status(200).json({"status" : "Atualizado com sucesso!"});
+  // Aqui a ordem é importante, para passar mais de um parâmetro usamos o array. Dentro dele a ordem importa, pois precisa corresponder ao SQL acima.
+  conexao.query(sql, [perfil, email], (erro, resultados) => {
+    if (erro) {
+      res.status(400).json(erro.code);
+    } else {
+      // res.status(200).json({"status" : "Atualizado com sucesso!"});
 
-            // spread operator (operador de "espalhamento" de objetos) pega todos os itens e adiciona dentro do mesmo objeto
-            res.status(200).json({...perfil, email});
-        }
-    });
+      // spread operator (operador de "espalhamento" de objetos) pega todos os itens e adiciona dentro do mesmo objeto
+      res.status(200).json({ ...perfil, email });
+    }
+  });
+}
+
+function atualizarEvento(id, evento, res) {
+  const sql = "UPDATE evento SET ? WHERE id = ?";
+
+  conexao.query(sql, [evento, id], (erro, resultados) => {
+    if (erro) {
+      res.status(400).json(erro.code);
+    } else {
+      // res.status(200).json({"status" : "Atualizado com sucesso!"});
+
+      // spread operator (operador de "espalhamento" de objetos) pega todos os itens e adiciona dentro do mesmo objeto
+      res.status(200).json({ ...evento, id });
+    }
+  });
 }
 
 // Função para excluir alunos
 function excluir(id, res) {
-    const sql = "DELETE FROM perfil WHERE id = ?";
+  const sql = "DELETE FROM perfil WHERE id = ?";
 
-    conexao.query(sql, id, (erro, resultados) => {
-        if(erro) {
-            res.status(400).json(erro.code);
-        } else {
-            res.status(200).json({"status" : "aluno excluído", id});
-        }
-    });
+  conexao.query(sql, id, (erro, resultados) => {
+    if (erro) {
+      res.status(400).json(erro.code);
+    } else {
+      res.status(200).json({ status: "perfil excluído", id });
+    }
+  });
 }
 
+function excluirEvento(id, res) {
+  const sql = "DELETE FROM evento WHERE id = ?";
 
+  conexao.query(sql, id, (erro, resultados) => {
+    if (erro) {
+      res.status(400).json(erro.code);
+    } else {
+      res.status(200).json({ status: "evento excluído", id });
+    }
+  });
+}
 
-
-export { ler, inserir, lerUm, atualizar, excluir };
-
-
+export {
+  ler,
+  inserir,
+  lerUm,
+  atualizar,
+  excluir,
+  lerEvento,
+  inserirEvento,
+  lerUmEvento,
+  atualizarEvento,
+  excluirEvento,
+};
